35d39496a0d49369e53fc2412d324fb3171e0cbd
Maiara Cristina Will <maiaracwill@gmail.com>
2022-11-12 16:23:51 -0300

    Created route that displays the predictions of a game of a given pool

--- src/routes/guess.ts
+++ src/routes/guess.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify'
-import {prisma} from '../lib/prisma'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { authenticate } from '../plugins/authenticate'
 
 export async function guessRoutes(fastify: FastifyInstance){
     
@@ -7,6 +9,83 @@ export async function guessRoutes(fastify: FastifyInstance){
 
         const count = await prisma.guess.count()
         return { count }
+    })
+
+    fastify.post('/pools/:poolId/games/:gameId/guesses', {
+        onRequest:[authenticate]
+    }, async (request, reply) =>{
+        const createGuessParams = z.object({
+            poolId: z.string(),
+            gameId: z.string(),
+        })
+
+        const createGuessBody = z.object({
+            firstTeamPoints: z.number(),
+            secondteamPoints: z.number(),
+        })
+
+        const {poolId, gameId} = createGuessParams.parse(request.params)
+        const {firstTeamPoints, secondteamPoints} = createGuessBody.parse(request.body)
+
+        const participant = await prisma.participant.findUnique({
+            where:{
+                userId_poolId:{
+                    poolId,
+                    userId: request.user.sub
+                }
+            }
+        })
+
+        if(!participant){
+            return reply.status(400).send({
+                message: "you're not allowed to create a guess inside this pool."
+            })
+        }
+
+        const guess = await prisma.guess.findUnique({
+            where:{
+                participantId_gameId:{
+                    participantId: participant.id,
+                    gameId
+                }
+            }
+        })
+
+        if(guess){
+            return reply.status(400).send({
+                message:"You already send a guess to this game on this pool."
+            })
+        }
+
+        const game = await prisma.game.findUnique({
+            where:{
+                id: gameId,
+            }
+        })
+
+        if(!game){
+            return reply.status(400).send({
+                message:"Game not found"
+            })
+        }
+
+        if(game.date < new Date()){
+            return reply.status(400).send({
+                message:"You cannot send guesses after the game date."
+            })
+
+        }
+
+        await prisma.guess.create({
+            data:{
+                gameId,
+                participantId: participant.id,
+                firstTeamPoints,
+                secondteamPoints
+            }
+        })
+
+        return reply.status(201).send()
     })
     
 }
